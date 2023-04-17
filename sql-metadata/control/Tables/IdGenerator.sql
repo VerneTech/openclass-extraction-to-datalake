f1fab50e98158999fd151bf25218752428ce1f5f
richardldiaz <richardldiaz@solidq.com>
2023-04-17 22:52:18 +0100

    Demo Terminada - versión básica

---
+++ sql-metadata/control/Tables/IdGenerator.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [control].[IdGenerator] (
+    [Id]         INT      IDENTITY (1, 1) NOT NULL,
+    [Created_on] DATETIME CONSTRAINT [DF_IdGenerator_Created_on] DEFAULT (getdate()) NOT NULL
+);
+
---
+++ sql-metadata/control/Tables/LastUpdate.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [control].[LastUpdate] (
+    [ID]                    INT           IDENTITY (1, 1) NOT NULL,
+    [DBName]                NVARCHAR (50) NULL,
+    [TableSchema]           NVARCHAR (50) NULL,
+    [TableName]             NVARCHAR (50) NULL,
+    [LastUpdate]            DATETIME      NULL,
+    [IncrementalDateColumn] NVARCHAR (50) NULL
+);
+
